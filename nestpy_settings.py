09632fe72fb7b206fefc128fb44bb35f2958625f
Jacob Rosenberg <jake@lendup.com>
2015-09-01 22:45:01 -0700

    move settings and make them env-based

---
+++ nestpy_settings.py
@@ -0,0 +1,8 @@
+import os
+
+## THESE auth urls are pulled from the Nest dev portal. 
+
+nest_auth_uri_1=os.environ['AUTHORIZATION_URL']
+nest_auth_uri_2=os.environ['ACCESS_TOKEN_URL']
+
+# http://localhost:5000/oauth2 <- Oauth redirect URL 
