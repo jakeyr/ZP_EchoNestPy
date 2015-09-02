e8d822bf69f01f2134df90a7641681b31752f446
Jacob Rosenberg <jake@lendup.com>
2015-09-01 22:58:29 -0700

    move to one app

--- nestpy.py
+++ nestpy.py
@@ -1,20 +1,21 @@
 import os
 import nestpy_app
 import nestpy_lib as nest
+import echopy_app
+import echopy_doc
+import echopy_nest as myApp
+import nestpy_app
 import nestpy_settings as settings
 
 import requests
 
 from flask import Flask, render_template, Response, send_from_directory, request, current_app, redirect, jsonify, json
 
-
 app = Flask(__name__)
-
 
 @app.route("/")
 def main():
 	return "Web Interface for NestPy"
-
 
 @app.route("/auth/<path:path>",methods = ['GET'])
 def auth(path):
@@ -53,20 +54,35 @@ def setMode(user,mode):
 	nest.setModeAll(user,mode)
 	return redirect("/")
 
+@app.route("/alexa/")
+def alexa_main():
+	return echopy_doc.main_page
 
+@app.route("/alexa/EchoPyAPI",methods = ['GET','POST'])
+def apicalls():
+	if request.method == 'POST':
+		data = request.get_json()
+		print "POST"
+		sessionId = myApp.data_handler(data)
+		return sessionId + "\n"
 
+@app.route("/alexa/auth/<path:path>",methods = ['GET'])
+def alexa_auth(path):
 
+	auth_uri = nest.nestAuth(path)
+	return redirect(auth_uri)
 
+@app.route("/alexa/oauth2",methods = ['GET'])
+def alexa_authcode():
+	user = request.args.get('state')
+	code = request.args.get('code')
 
+	if nest.nestToken(user,code):
 
+		print nest.nestData.getUser(user).getToken()
 
-def run_nestpy_app():
-	import SocketServer
-	#SocketServer.BaseServer.handle_error = close_stream
-	SocketServer.ThreadingTCPServer.allow_reuse_address = True
-	nestpy_app.run(app)
+	return redirect("/alexa")
 
-
-if __name__ == "__main__":
-	nest.nestDataStoreInit()
-	run_nestpy_app()
+if __name__ == '__main__':
+  port = int(os.environ.get('PORT',5000))
+  app.run(host='0.0.0.0', port=port)
